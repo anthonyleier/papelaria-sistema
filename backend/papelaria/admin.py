8a2a0666cc9c07b44957da9e897c074236897163
Anthony Cruz <anthonyleierlw@gmail.com>
2023-10-20 14:30:37 -0300

    Registro das models no admin

--- backend/papelaria/admin.py
+++ backend/papelaria/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Cliente, Vendedor, Produto, DiaDaSemana, Venda, ItemVenda
 
-# Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'telefone')
+
+
+class VendedorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'telefone')
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'descricao', 'valor_unitario', 'percentual_comissao')
+
+
+class DiaDaSemanaAdmin(admin.ModelAdmin):
+    list_display = ('dia', 'percentual_minimo', 'percentual_maximo')
+
+
+class ItemVendaInline(admin.TabularInline):
+    model = ItemVenda
+
+
+class VendaAdmin(admin.ModelAdmin):
+    list_display = ('numero_nota_fiscal', 'data_hora', 'cliente', 'vendedor')
+    inlines = [ItemVendaInline]
+
+
+class ItemVendaAdmin(admin.ModelAdmin):
+    list_display = ('venda', 'produto', 'quantidade', 'comissao')
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Vendedor, VendedorAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(DiaDaSemana, DiaDaSemanaAdmin)
+admin.site.register(Venda, VendaAdmin)
+admin.site.register(ItemVenda, ItemVendaAdmin)
