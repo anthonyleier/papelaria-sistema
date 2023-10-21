8c898e6060c19a43de99ee1002c23a205ff1ac14
Anthony Cruz <anthonyleierlw@gmail.com>
2023-10-21 18:34:05 -0300

    Dockerfile para o projeto Django

--- backend/afazer.md
+++
@@ -1,7 +0,0 @@
-- Colocar docker
-- Colocar postgres
-
-- Revisar documentação, testes, histórico de commits, design e boas práticas de engenharia de software
-- Revisar conceitos do The Twelve-Factor App
-- Revisar boas prácticas de programação
-- Revisar princípios do SOLID
--- backend/projeto/settings.py
+++ backend/projeto/settings.py
@@ -23,10 +23,10 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'insecure')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
