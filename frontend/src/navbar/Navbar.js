09a8c9d8dc5fe1c893fede3edc04247e992d4ecd
Anthony Cruz <anthonyleierlw@gmail.com>
2023-10-23 22:53:22 -0300

    Incluido Menu Lateral na Navbar

--- frontend/src/navbar/Navbar.js
+++ frontend/src/navbar/Navbar.js
@@ -1,46 +1,41 @@
 import React, { useState } from "react";
-import { FaShoppingCart, FaDollarSign, FaArrowRight } from "react-icons/fa";
+import { FaShoppingCart, FaDollarSign, FaArrowRight, FaWindowClose } from "react-icons/fa";
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 import "./navbar.css";
 
 const Navbar = ({ tituloDaPagina }) => {
-  const [menuAberto, setMenuAberto] = useState(false);
+    const [menuAberto, setMenuAberto] = useState(true);
 
-  const toggleMenu = () => {
-    setMenuAberto(!menuAberto);
-  };
+    const toggleMenu = () => {
+        setMenuAberto(!menuAberto);
+    };
 
-  return (
-    <nav className="navbar">
-      <button className="menu-button" onClick={toggleMenu}>
-        <FaBars />
-      </button>
-      {menuAberto && (
-        <div className="menu-lateral">
-          <ul>
-            <li>
-              <Link to="/vendas">
-                <FaShoppingCart />
-                Vendas
-                <FaArrowRight />
-              </Link>
-            </li>
-            <li>
-              <Link to="/comissoes">
-                <FaDollarSign />
-                Comissões
-                <FaArrowRight />
-              </Link>
-            </li>
-          </ul>
-        </div>
-      )}
-      <img src="/logo.svg" alt="Logo da Empresa" className="logo" />
-      <h1 className="titulo-pagina">{tituloDaPagina}</h1>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <button className="menu-button" onClick={toggleMenu}>
+                <FaBars />
+            </button>
+            {menuAberto && (
+                    <div className="menu-aberto">
+                    <FaWindowClose onClick={toggleMenu} className="menu-button-fechar" />
+                        <Link to="/vendas" className="menu-link">
+                            <div className="menu-item">
+                                <FaShoppingCart /> <p>Vendas</p> <FaArrowRight />
+                            </div>
+                        </Link>
+                        <Link to="/comissoes" className="menu-link">
+                            <div className="menu-item">
+                                <FaDollarSign /> <p>Comissões</p> <FaArrowRight />
+                            </div>
+                        </Link>
+                    </div>
+            )}
+            <img src="/logo.svg" alt="Logo da Empresa" className="logo" />
+            <h1 className="titulo-pagina">{tituloDaPagina}</h1>
+        </nav>
+    );
 };
 
 export default Navbar;
