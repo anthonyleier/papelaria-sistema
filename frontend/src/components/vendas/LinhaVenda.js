288abe8edc9a1ddfe9cbfcd166f6dd8cd9e8d4c4
Anthony Cruz <anthonyleierlw@gmail.com>
2023-10-24 15:12:21 -0300

    Remover imports não usados

--- frontend/src/components/vendas/LinhaVenda.js
+++ frontend/src/components/vendas/LinhaVenda.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { RiEditBoxLine } from "react-icons/ri";
 import { FaTrash } from "react-icons/fa";
 
